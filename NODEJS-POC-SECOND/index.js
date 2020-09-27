f65fd0d57b44a9d259409d843a262f883d4ad76d
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-09-27 18:49:24 +0530

    add blog app

---
+++ NODEJS-POC-SECOND/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const { config, engine } = require('express-edge');
+//const expressEdge = require('express-edge');
+
+const app = new express();
+
+app.use(express.static('public'));
+//app.use(expressEdge);
+app.use(engine);
+app.set('views', `${__dirname}/views`);
+
+app.get('/', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  res.render('index');
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+})
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+})
+
+app.listen(4000, () => {
+  console.log('App listening at 4000');
+})
