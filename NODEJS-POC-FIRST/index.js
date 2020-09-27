edd68bbb0cc08c2d36d8739e3e779a23005ef3a5
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-09-27 13:31:26 +0530

    install express

--- NODEJS-POC-FIRST/index.js
+++ NODEJS-POC-FIRST/index.js
@@ -1,16 +1,22 @@
 const http = require('http');
 
+const fs = require('fs'); //fs - file system
+const aboutPage = fs.readFileSync('about.html');
+const homePage = fs.readFileSync('index.html');
+
 const server = http.createServer((request, response) => {
   console.log(request.url);
 
   if (request.url === '/about') {
-    return response.end('The ABOUT PAGE');
+    // return response.end('The ABOUT PAGE');
+    return response.end(aboutPage);
   }
   else if (request.url === '/contact') {
     return response.end('The Contact PAGE');
   }
   else if (request.url === '/') {
-    return response.end('This is my Home page');
+    //return response.end('This is my Home page');
+    return response.end(homePage);
   }
   else {
     //for status 404, if remove this line then it will be 200
