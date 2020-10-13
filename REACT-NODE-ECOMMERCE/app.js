9228d58dba8a4662a9176a8049839d5923ad7723
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-10-14 00:25:30 +0530

    monoDB added

--- REACT-NODE-ECOMMERCE/app.js
+++ REACT-NODE-ECOMMERCE/app.js
@@ -1,7 +1,33 @@
-const express = require('express')
-const app = express();
-require('dotenv').config();
+// import mongoose
+const mongoose = require('mongoose');
+const express = require('express');
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config();
 
+//app
+const app = express();
+
+
+
+//require('dotenv').config();
+
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }
+)
+  .then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
+
+//routes
 app.get('/', (req, res) => {
   res.send('hello from node');
 });
