d0b89ca433b7327a843907692d61c899d7b080d5
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-10-21 01:49:46 +0530

    added signup layout

---
+++ ecommerce-frontend/src/user/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../core/Layout';
+import { API } from '../config';
+
+
+
+const Signup = () => (
+  <Layout title='Home Page' description='Signup to Node React E-commerce App'>
+    {API}
+  </Layout>
+);
+
+export default Signup;
